f7aa57e75ba12cb9441e72d7ba6a7bb286b7d3e7
gsvsantos <gustavocnsantos02@gmail.com>
2025-12-19 11:35:44 -0300

    refactor: Atualiza lista de skills icons.

--- src/app/utils/lista-icones-habilidade.ts
+++ src/app/utils/lista-icones-habilidade.ts
@@ -2,53 +2,75 @@ import { IconeHabilidade } from '../models/icone-habilidade';
 
 export const iconesHabilidades: IconeHabilidade[] = [
   {
-    urlImagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dot-net/dot-net-original.svg',
+    urlImagem:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dot-net/dot-net-original.svg',
     titulo: '.NET',
-    tipo: 'iconeHabilidade'
+    tipo: 'iconeHabilidade',
   },
   {
-    urlImagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg',
+    urlImagem:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg',
     titulo: 'TypeScript',
-    tipo: 'iconeHabilidade'
+    tipo: 'iconeHabilidade',
   },
   {
     urlImagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg',
     titulo: 'HTML5',
-    tipo: 'iconeHabilidade'
+    tipo: 'iconeHabilidade',
   },
   {
     urlImagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg',
     titulo: 'CSS3',
-    tipo: 'iconeHabilidade'
+    tipo: 'iconeHabilidade',
   },
   {
     urlImagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg',
     titulo: 'Sass',
-    tipo: 'iconeHabilidade'
+    tipo: 'iconeHabilidade',
   },
   {
-    urlImagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg',
+    urlImagem:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg',
     titulo: 'Angular',
-    tipo: 'iconeHabilidade'
+    tipo: 'iconeHabilidade',
   },
   {
     urlImagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rxjs/rxjs-original.svg',
     titulo: 'RxJS',
-    tipo: 'iconeHabilidade'
+    tipo: 'iconeHabilidade',
   },
   {
-    urlImagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg',
+    urlImagem:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg',
     titulo: 'Postgres',
-    tipo: 'iconeHabilidade'
+    tipo: 'iconeHabilidade',
+  },
+  {
+    urlImagem:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/microsoftsqlserver/microsoftsqlserver-original.svg',
+    titulo: 'SQL Server',
+    tipo: 'iconeHabilidade',
+  },
+  {
+    urlImagem:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dbeaver/dbeaver-original.svg',
+    titulo: 'DBeaver',
+    tipo: 'iconeHabilidade',
+  },
+  {
+    urlImagem:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg',
+    titulo: 'Postman',
+    tipo: 'iconeHabilidade',
   },
   {
     urlImagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redis/redis-original.svg',
     titulo: 'Redis',
-    tipo: 'iconeHabilidade'
+    tipo: 'iconeHabilidade',
   },
   {
     urlImagem: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-plain.svg',
     titulo: 'Docker',
-    tipo: 'iconeHabilidade'
+    tipo: 'iconeHabilidade',
   },
 ];
